9ed50fb972ff5c16ac0cdc9ec9874e1db3054549
global2alex <global2alex@gmail.com>
2017-11-26 16:12:20 -0500

    Fixed audioplayback

--- js/script.js
+++ js/script.js
@@ -25,16 +25,15 @@ function MAKE_A_SOUND(file)
 
 document.addEventListener('keypress', function(LISTEN)
 {
-	if ( (LISTEN.keyCode == 13 || LISTEN.which == 13) && audio_playing === false)
+	if ( (LISTEN.keyCode == 13 || LISTEN.which == 13) && audio_playing === false )
 	{
 		MAKE_A_SOUND(audio_file);
 		audio_playing = true;
 	}
-	else if (LISTEN.keyCode == 32 || LISTEN.which == 32)
+	else if ( (LISTEN.keyCode == 32 || LISTEN.which == 32) && audio_playing === true )
 	{
 		clicked_time = Date.now();
 		reaction_time = (clicked_time - loaded_time) / 1000;
-		// difference_time = (clicked_time - created_time) / 1000;
 		obj.table.push(' ' + reaction_time);
 		for (const prop in obj)
 		{
