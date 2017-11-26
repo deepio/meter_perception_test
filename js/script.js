fbaec9cde3c56264707437200b553ab33e8d60c9
global2alex <global2alex@gmail.com>
2017-11-26 16:04:16 -0500

    Fix audio multi-playback.
    New audio would be initiated when another was already playing.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
 var clicked_time; var created_time; var loaded_time; var reaction_time; var difference_time;
+var audio_file = 'audio/1.mp3';
+var audio_playing = false;
 var obj =
 {
 	table: []
 };
 
-function MAKE_A_SOUND()
+function MAKE_A_SOUND(file)
 {
-	var audio = new Audio('audio/1.mp3');
-	created_time = Date.now();
+	var audio = new Audio(file);
 	audio.play();
 	loaded_time = Date.now();
+	audio.addEventListener("ended", function(){
+	     audio.currentTime = 0;
+	     audio_playing = false;
+	});
 }
 
 //
 
 document.addEventListener('keypress', function(LISTEN)
 {
-	if (LISTEN.keyCode == 32 || LISTEN.which == 32)
+	if ( (LISTEN.keyCode == 13 || LISTEN.which == 13) && audio_playing === false)
+	{
+		MAKE_A_SOUND(audio_file);
+		audio_playing = true;
+	}
+	else if (LISTEN.keyCode == 32 || LISTEN.which == 32)
 	{
 		clicked_time = Date.now();
 		reaction_time = (clicked_time - loaded_time) / 1000;
-		difference_time = (clicked_time - created_time) / 1000;
+		// difference_time = (clicked_time - created_time) / 1000;
 		obj.table.push(' ' + reaction_time);
 		for (const prop in obj)
 		{
 			document.getElementById('print_all_times').innerHTML = `obj.${prop} = ${obj[prop]}`;
-			document.getElementById('print_latest_time').innerHTML = difference_time;
+			// document.getElementById('print_latest_time').innerHTML = difference_time;
 		}
-	}
-	else if (LISTEN.keyCode == 13 || LISTEN.which == 13)
-	{
-		MAKE_A_SOUND();
 	}
 	else {}
 });
