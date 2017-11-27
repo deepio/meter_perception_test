610664709fb9b07b59d9c887fbb864e44cbfa3b9
global2alex <global2alex@gmail.com>
2017-11-26 20:02:33 -0500

    Add clear variable names.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-var clicked_time;
-var loaded_time;
+var trial_start_time;
+var audio_loaded_time;
 var reaction_time;
-var trials = 0;
+var current_trial = 0;
 var audio_tree = ['audio/1.mp3', 'audio/2.mp3', 'audio/3.mp3'];
 var audio_playing = false;
 var random_file = RANDOMIZE_FILE_ORDER(audio_tree)[0];
-var obj =
-{
-	table: []
-};
+var trial = { rhythm_table: [] };
 
 document.getElementById('send').style.display="none";
 
@@ -16,20 +13,19 @@ function MAKE_A_SOUND(file)
 {
 	var audio = new Audio(file);
 	audio.play();
-	loaded_time = Date.now();
+	audio_loaded_time = Date.now();
 	audio.addEventListener("ended", function()
 	{
 	     audio.currentTime = 0;
 	     audio_playing = false;
-			 trials ++;
-			 if ( trials == audio_tree.length ) {document.getElementById('send').style.display="block";}
+			 current_trial ++;
+			 if ( current_trial == audio_tree.length ) {document.getElementById('send').style.display="block";}
 	});
 }
 
 function RANDOMIZE_FILE_ORDER(file_list)
 {
 	var output = [];
-
 	for (i = 0; i < file_list.length; i++)
 	{
 		var a = Math.trunc( Math.random() * ((file_list.length) - 0) );
@@ -43,7 +39,6 @@ function RANDOMIZE_FILE_ORDER(file_list)
 			}
 		}
 	}
-
 	return [output];
 }
 
@@ -57,16 +52,16 @@ document.addEventListener('keypress', function(LISTEN)
 		MAKE_A_SOUND(audio_file);
 		audio_playing = true;
 		random_file.splice(0, 1);
-		obj.table.push(audio_file);
+		trial.rhythm_table.push(audio_file);
 	}
 	else if ( (LISTEN.keyCode == 32 || LISTEN.which == 32) && audio_playing === true )
 	{
-		clicked_time = Date.now();
-		reaction_time = (clicked_time - loaded_time) / 1000;
-		obj.table.push(' ' + reaction_time);
-		for (var prop in obj)
+		trial_start_time = Date.now();
+		reaction_time = (trial_start_time - audio_loaded_time) / 1000;
+		trial.rhythm_table.push(' ' + reaction_time);
+		for (var prop in trial)
 		{
-			document.getElementById('print_all_times').innerHTML = `obj.${prop} = ${obj[prop]}`;
+			document.getElementById('print_all_times').innerHTML = `trial.${prop} = ${trial[prop]}`;
 		}
 	}
 	else {}
