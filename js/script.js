39abcb80babd0944d207cb84564fc52f1cbbf6c6
global2alex <global2alex@gmail.com>
2017-11-26 19:09:28 -0500

    Added stimuli randomizer.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ var loaded_time;
 var reaction_time;
 var difference_time;
 var audio_file = 'audio/1.mp3';
+var audio_tree = ['audio/1.mp3', 'audio/2.mp3', 'audio/3.mp3'];
 var audio_playing = false;
+var random_file = RANDOMIZE_FILE_ORDER(audio_tree)[0];
 var obj =
 {
 	table: []
@@ -24,14 +26,38 @@ function MAKE_A_SOUND(file)
 	});
 }
 
-//
+function RANDOMIZE_FILE_ORDER(file_list)
+{
+	var output = [];
+
+	for (i = 0; i < file_list.length; i++)
+	{
+		var a = Math.trunc( Math.random() * ((file_list.length) - 0) );
+		output.push(a);
+		for (j = 0; j < i; j++)
+		{
+			if ( output[i] == output[j] )
+			{
+				output.pop();
+				i--;
+			}
+		}
+	}
+
+	return [output];
+}
 
 document.addEventListener('keypress', function(LISTEN)
 {
 	if ( (LISTEN.keyCode == 13 || LISTEN.which == 13) && audio_playing === false )
 	{
+		audio_file = audio_tree[random_file[0]];
+		console.log('Playing: ' + audio_file);
+
 		MAKE_A_SOUND(audio_file);
 		audio_playing = true;
+		random_file.splice(0, 1);
+		obj.table.push(audio_file);
 	}
 	else if ( (LISTEN.keyCode == 32 || LISTEN.which == 32) && audio_playing === true )
 	{
